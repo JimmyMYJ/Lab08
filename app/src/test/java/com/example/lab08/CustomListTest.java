590d8b952e7d3944b42c3d5f96e48a82e1407758
JimmyMYJ <144820759+JimmyMYJ@users.noreply.github.com>
2025-10-31 12:39:12 -0600

    commit

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -22,5 +22,16 @@ public class CustomListTest {
         assertFalse(list.hasCity(calgary)); // Should be gone
     }
 
+    @Test
+    public void testCountCities() {
+        CustomList list = new CustomList();
+        assertEquals(0, list.countCities());
+
+        list.addCity(new City("Calgary", "AB"));
+        list.addCity(new City("Edmonton", "AB"));
+        assertEquals(2, list.countCities());
+    }
+
+
 
 }
